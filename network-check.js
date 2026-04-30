3f237125f666c42619ecc5c8c3262cdcafa6d155
codecrafte-org <codecrafte07@gmail.com>
2026-04-30 22:46:51 +0500

    save

--- network-check.js
+++ network-check.js
@@ -1,8 +1,17 @@
+// network-check.js
 function updateOnlineStatus() {
     if (!navigator.onLine) {
-        // replace use karne se user back button dabayega toh stuck nahi hoga
-        window.location.replace("error404.html");
+        // Current page ka pura URL save karo taake wapsi sahi jagah ho
+        sessionStorage.setItem('last_url', window.location.href);
+        
+        // "/" lagane se ye har folder se hamesha Root folder mein error page dhoonde ga
+        window.location.replace("/error404.html"); 
     }
 }
 
 window.addEventListener('offline', updateOnlineStatus);
+
+// Initial check agar page load hote hi net na ho
+if (!navigator.onLine) {
+    updateOnlineStatus();
+}
